b319c6b63be72440361255f34830a4f435703557
HitteAnzo <hitteanzo@gmail.com>
2024-11-11 14:40:36 +0100

    add Pricing class with validation for amount and currency; add SkyHighBuilding class extending Building with floors validation

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,33 +1,49 @@
+/* eslint-disable */
+import Currency from './3-currency.js';
 
-export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
-    }
-
-    get code() {
-        return this._code;
-    }
-
-    set code(newCode) {
-        if (typeof newCode !== 'string') {
-            throw new TypeError('Code must be a string');
+export default class Pricing {
+    constructor(amount, currency) {
+        if (typeof amount !== 'number') {
+            throw new TypeError('Amount must be a number');
         }
-        this._code = newCode;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set name(newName) {
-        if (typeof newName !== 'string') {
-            throw new TypeError('Name must be a string');
+        if (!(currency instanceof Currency)) {
+            throw new TypeError('Currency must be an instance of Currency');
         }
-        this._name = newName;
+
+        this._amount = amount;
+        this._currency = currency;
     }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(newAmount) {
+        if (typeof newAmount !== 'number') {
+            throw new TypeError('Amount must be a number');
+        }
+        this._amount = newAmount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(newCurrency) {
+        if (!(newCurrency instanceof Currency)) {
+            throw new TypeError('Currency must be an instance of Currency');
+        }
+        this._currency = newCurrency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+            throw new TypeError('Both amount and conversionRate must be numbers');
+        }
+        return amount * conversionRate;
     }
 }
