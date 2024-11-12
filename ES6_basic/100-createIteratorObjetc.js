67d6077467d40cfa28cacf7bf2df2580a8b14bd1
HitteAnzo <hitteanzo@gmail.com>
2024-11-12 13:03:09 +0100

    add 100

---
+++ ES6_basic/100-createIteratorObjetc.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function createIteratorObject(report) {
+    const allEmployees = [];
+  
+    for (const department in report.allEmployees) {
+      if (report.allEmployees.hasOwnProperty(department)) {
+        allEmployees.push(...report.allEmployees[department]);
+      }
+    }
+
+    return allEmployees[Symbol.iterator]();
+  }
+  
