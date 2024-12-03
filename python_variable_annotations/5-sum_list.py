5436050b0edf15ce5ec865a8edb54cea10715ce4
HitteAnzo <hitteanzo@gmail.com>
2024-12-03 03:22:49 +0100

    feat: add module docstring and function docstring for sum_list in 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+""" Module to sum a list of floats. """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Return the sum of a list of floats.
+
+    Args:
+    input_list (List[float]): The list of float numbers.
+
+    Returns:
+    float: The sum of the float numbers in the list.
+    """
+    return sum(input_list)
