b055bf7f814687d782f891e23f5ddf74e359be2c
HitteAnzo <hitteanzo@gmail.com>
2024-12-03 03:24:57 +0100

    feat: add module docstring and function docstring for sum_mixed_list in 6-sum_mixed_list.py

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""" Sum mixed list """
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Return the sum of a list of integers and floats.
+
+    Args:
+    mxd_lst (List[Union[int, float]]): The list of integers and float numbers.
+
+    Returns:
+    float: The sum of the numbers in the list.
+    """
+    return sum(mxd_lst)
