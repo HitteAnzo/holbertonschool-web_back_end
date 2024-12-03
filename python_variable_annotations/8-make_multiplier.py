0d0cf744fbec879413013b4f3297b386980cbee6
HitteAnzo <hitteanzo@gmail.com>
2024-12-03 03:29:57 +0100

    feat: add module and function docstrings for make_multiplier in 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+""" Module for element_length function """
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Return a function that multiplies a float by the given multiplier.
+
+    Args:
+    multiplier (float): The multiplier.
+
+    Returns:
+    Callable[[float], float]: A function that multiplies a float by the multiplier.
+    """
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+    
+    return multiplier_function
