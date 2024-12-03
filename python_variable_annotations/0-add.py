140d02e64122aedb71afe6f96460db8e63985bcb
HitteAnzo <hitteanzo@gmail.com>
2024-12-03 03:13:09 +0100

    feat: add add function to perform addition of two float numbers with type annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return their sum.
+
+    Args:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
