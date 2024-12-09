7e7f667753a46c3167cade7d3eff826fe1a554a7
HitteAnzo <hitteanzo@gmail.com>
2024-12-09 04:46:32 +0100

    fix: add blank line for improved readability in async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,6 +10,7 @@ async def async_generator() -> Generator[float, None, None]:
     Coroutine that yields a random number between 0 and 10, 10 times,
     each time asynchronously waiting for 1 second.
     """
+
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
