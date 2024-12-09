eb9b8a25901c26dcc61bb4da2dd67d2a5eaa1073
HitteAnzo <hitteanzo@gmail.com>
2024-12-09 10:58:57 +0100

    feat: add simple helper function for pagination and async generator example

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+""" Async generator """
+
 import asyncio
 import random
 from typing import Generator
-""" Async generator """
 
 
 async def async_generator() -> Generator[float, None, None]:
