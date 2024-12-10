e33c51aeaa25d2cf36f842d3b68ca8627d72a920
HitteAnzo <hitteanzo@gmail.com>
2024-12-10 11:34:14 +0100

    fix: correct method name from updateMany to update_many in update_topics function

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -8,5 +8,5 @@ db = client.mydatabase
 mongo_collection = db.school
 
 def update_topics(mongo_collection, name, topics):
-    """ changes all topics of a school document based on the name """
-    return mongo_collection.updateMany({"name": name}, {"$set": {"topics": topics}})
+    """ Changes all topics of a school document based on the name """
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
