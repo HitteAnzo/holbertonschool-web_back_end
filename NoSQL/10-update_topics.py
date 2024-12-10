458bdff9476e60e4b6036a0aa3b438bba67f9133
HitteAnzo <hitteanzo@gmail.com>
2024-12-10 11:07:28 +0100

    feat: add function to update topics for school documents in the collection

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+""" Update a document in Python """
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.mydatabase
+mongo_collection = db.school
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document based on the name """
+    return mongo_collection.updateMany({"name": name}, {"$set": {"topics": topics}})
