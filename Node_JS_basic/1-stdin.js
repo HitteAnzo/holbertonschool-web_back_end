d368db91b9cc24aa91e810201a630d82e1530a2c
HitteAnzo <hitteanzo@gmail.com>
2025-01-20 11:01:42 +0100

    fix: ensure closing message is displayed on process end in stdin module

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,12 +6,13 @@
 // Prompt the user for their name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  // Display the user's name
   process.stdout.write(`Your name is: ${name}`);
-  // Display the closing message
-  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
+
+process.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
