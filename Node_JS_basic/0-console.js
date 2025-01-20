739edc81890599dd24bbf3137c35a7fd919e9adb
HitteAnzo <hitteanzo@gmail.com>
2025-01-20 09:39:05 +0100

    feat: update .gitignore to include database.csv and add initial files for console and README

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints the given message to STDOUT.
+ * @param {string} message - The message to print.
+ */
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
