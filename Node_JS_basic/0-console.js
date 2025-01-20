7ebf992ed5911f8934e4dc6b1cfdc1b23ad40213
HitteAnzo <hitteanzo@gmail.com>
2025-01-20 09:59:09 +0100

    fix: correct indentation in displayMessage function and ensure proper export

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -4,8 +4,8 @@
  * @param {string} message - The message to be displayed.
  */
 function displayMessage(message) {
-    console.log(message);
-  }
-  
-  // Export the displayMessage function to be used in other modules.
-  module.exports = displayMessage;
+  console.log(message);
+}
+
+// Export the displayMessage function to be used in other modules.
+module.exports = displayMessage;
