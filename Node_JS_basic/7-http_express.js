dba90e24a860d75a3d42a693feab00d0cb2650c2
HitteAnzo <hitteanzo@gmail.com>
2025-01-20 14:52:04 +0100

    refactor: switch from ES6 to CommonJS syntax for compatibility with existing code

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import countStudents from './3-read_file_async';
+const express = require('express');
 
 const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
 const DATABASE = args[0];
 
 const app = express();
 const port = 1245;
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (_, res) => {
+app.get('/students', async (req, res) => {
   const msg = 'This is the list of our students\n';
   try {
     const students = await countStudents(DATABASE);
