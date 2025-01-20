659ad2d4a9ad6998c6bb015742d05ed4ffd4ad46
HitteAnzo <hitteanzo@gmail.com>
2025-01-20 10:33:07 +0100

    fix: streamline user input handling and exit process in stdin module

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,8 @@
-// Prompt the user for their name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for data input from the user
 process.stdin.on('data', (data) => {
-  // Convert the input data to a string and trim any extra whitespace
   const name = data.toString().trim();
-  
-  // Output the user's name
   process.stdout.write(`Your name is: ${name}\n`);
-  
-  // Inform the user that the software is closing
   process.stdout.write('This important software is now closing\n');
-
-  
-  // End the input stream
-  process.stdin.end();
+  process.exit();
 });
