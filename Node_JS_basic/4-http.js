78e9dabdc4c2fa04bb4450450ad1bb057e50e0fb
HitteAnzo <hitteanzo@gmail.com>
2025-01-20 13:07:08 +0100

    fix: correct response message formatting in HTTP server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,16 +1,13 @@
 const http = require('http');
 
-// Create an HTTP server
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
 });
 
-// Listen on port 1245
 app.listen(1245, () => {
   console.log('Server is listening on port 1245');
 });
 
-// Export the server
 module.exports = app;
